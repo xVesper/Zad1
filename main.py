d2af065a4831194fa353d186f2180f7833ffa8de
xVesper <florian.jasiuczenia@gmail.com>
2021-02-25 11:09:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+#zad1
+# napis = "Witam"
+# napis1 = "wszystkich"
+#
+# print(napis,napis1)
+#
+# a = 4
+# b = 5
+#
+# print(a,b)
+#zad2
+# from math import *
+# a = 10
+# b = 5
+# c = 2
+#
+# mnozenie = a*b
+# print(mnozenie)
+# dzielenie = a/b
+# print(dzielenie)
+# dodawanie = a+c
+# print(dodawanie)
+# odejmowanie = c-b
+# print(odejmowanie)
+# potegowanie = a**c
+# print(potegowanie)
+# reszta = a%b
+# print(reszta)
+# pierwiastek = sqrt(16)
+# print(pierwiastek)
+#zad3
+# a = 5
+# print(a)
+# a += 5
+# print(a)
+# a -= 5
+# print(a)
+# a *= 5
+# print(a)
+# a /= 5
+# print(a)
+# a **= 5
+# print(a)
+# a %= 5
+# print(a)
+#zad4
+from math import *
+e = e ** 10
+
+print(e)
+
+x=(log(5 + sin(8)**2))
+y=pow(x,1/6)
+
+print(y)
+
+
+
+
+
+
+
+
+
