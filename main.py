2a37ffc460ea42dc02100b2d3fb23427f60e836e
xVesper <florian.jasiuczenia@gmail.com>
2021-02-28 20:31:52 +0100

    Praca domowa 1

--- main.py
+++ main.py
@@ -44,17 +44,65 @@
 # a %= 5
 # print(a)
 #zad4
-from math import *
-e = e ** 10
-
-print(e)
-
-x=(log(5 + sin(8)**2))
-y=pow(x,1/6)
-
-print(y)
-
-
+# from math import *
+# e = e ** 10
+#
+# print(e)
+#
+# x=(log(5 + sin(8)**2))
+# y=pow(x,1/6)
+#
+# print(y)
+#
+# z = [3,55]
+#
+# print(min(z))
+#
+# v = [4,80]
+#
+# print(max(v))
+# #zad5
+# imie = "KACPER"
+# nazwisko = "JASIUCZENIA"
+#
+# capitalize1 = imie.capitalize()
+# capitalize2 = nazwisko.capitalize()
+# print(capitalize1,capitalize2)
+#
+# #zad6
+#
+# piosenka = "Do nieba nie chodze, bo jest mi nie po drodze, la la la"
+#
+# c = piosenka.count("la")
+#
+# print("ilosc la :",c)
+#
+# #zad7
+#
+# zmiena = "Kacper Jasiuczenia"
+#
+# pierwsza = zmiena[1]
+# ostatnia = zmiena[17]
+#
+# print(pierwsza,ostatnia)
+#
+# #zad8
+#
+# split = piosenka.split()
+#
+# print(split)
+#
+# #zad9
+#
+# string = "Kacper"
+#
+# print(string)
+#
+# print(float(10))
+#
+# numer = 12345
+#
+# print(numer,"w szesnastkowym",hex(numer))
 
 
 
